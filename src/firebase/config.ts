75bf357f88013671c5bcb8195622f109142f8a23
Luis Bautista <luis.bautista@uai.edu.pe>
2025-10-13 21:02:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9547505888-97bc1",
+  "appId": "1:496046852621:web:d7a1e7ec8e6080b1eba344",
+  "apiKey": "AIzaSyCYxf4wh1F1Z0JTyleqgm-5THMgufzJE5Q",
+  "authDomain": "studio-9547505888-97bc1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "496046852621"
+};
